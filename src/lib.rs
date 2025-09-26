5421282877de77271b0890bd9baeb9b6b8d41af4
Matthew Franchini <matchewf11@gmail.com>
2025-09-25 19:31:24 -0700

    hello

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,289 @@
+use rusqlite::{Connection, Result};
+
+pub struct Conn {
+    client: Connection,
+}
+
+impl Conn {
+    pub fn build(file_path: &str) -> Result<Self> {
+        let client = Connection::open(file_path)?;
+
+        client.execute(
+            r"
+            CREATE TABLE IF NOT EXISTS categories (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL UNIQUE CHECK(name != '')
+            )
+            ",
+            (),
+        )?;
+
+        client.execute(
+            r"
+            CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY,
+                info TEXT NOT NULL CHECK(info != ''),
+                done BOOLEAN NOT NULL DEFAULT false CHECK(done IN (0,1)),
+                due_date TEXT CHECK(
+                    due_date IS NULL OR
+                    (due_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]' AND
+                        date(due_date) IS NOT NULL)
+            ),
+            category INTEGER,
+            FOREIGN KEY(category) REFERENCES categories(id)
+            )",
+            (),
+        )?;
+
+        Ok(Self { client })
+    }
+}
+// pub fn format_date(date: &str, today: &NaiveDate) -> Result<String, &'static str> {
+//     let cleaned = date.trim().replace("/", "-");
+//     let parts: Vec<_> = cleaned.trim().split('-').collect();
+//
+//     // let parse_part = |part: &str, msg| part.parse::<u32>().map_err(|_| msg);
+//     // let (y, m, d) = match parts.len() {
+//     //     3 => (
+//     //         Some(parse_part(parts[0], "Y-M-D invalid year")?),
+//     //         Some(parse_part(parts[1], "Y-M-D invalid month")?),
+//     //         Some(parse_part(parts[2], "Y-M-D invalid day")?),
+//     //     ),
+//     //     2 => (
+//     //         None,
+//     //         Some(parse_part(parts[0], "M-D invalid month")?),
+//     //         Some(parse_part(parts[1], "M-D invalid day")?),
+//     //     ),
+//     //     1 => (None, None, Some(parse_part(parts[0], "D invalid day")?)),
+//     //     _ => return Err("bad number of parts"),
+//     // };
+//     //
+//     Ok(cleaned)
+//
+//     // fn make_date(y: i32, m: u32, d: u32) -> Result<String, &'static str> {
+//     //     if let Some(date) = NaiveDate::from_ymd_opt(y, m, d) {
+//     //         Ok(date.format("%Y-%m-%d").to_string())
+//     //     } else {
+//     //         Err("unable to parse date")
+//     //     }
+//     // }
+//     //
+//     // match (y, m, d) {
+//     //     // wont work past 2100 :(, will fix then
+//     //     (Some(year), Some(month), Some(day)) => make_date(
+//     //         (if year < 100 { year + 2000 } else { year }) as i32,
+//     //         month,
+//     //         day,
+//     //     ),
+//     //     (None, Some(month), Some(day)) => {
+//     //         if month > today.month() || (month == today.month() && day >= today.day()) {
+//     //             make_date(today.year(), month, day)
+//     //         } else {
+//     //             make_date(today.year() + 1, month, day)
+//     //         }
+//     //     }
+//     //     (None, None, Some(day)) => {
+//     //         if day >= today.day() {
+//     //             make_date(today.year(), today.month(), day)
+//     //         } else {
+//     //             let today_month = today.month();
+//     //             let (new_year, new_month) = if today_month == 12 {
+//     //                 (today.year() + 1, 1)
+//     //             } else {
+//     //                 (today.year(), today_month + 1)
+//     //             };
+//     //             make_date(new_year, new_month, day)
+//     //         }
+//     //     }
+//     //     _ => Err("cannot parse the date"),
+//     // }
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rusqlite::{Error::SqliteFailure, Result, ffi};
+
+    // #[test]
+    // fn format_date_test() {
+    //     unimplemented!();
+    // }
+
+    fn assert_err<T>(res: Result<T>, err_code: i32, err_msg: &str) {
+        match res {
+            Err(SqliteFailure(err, msg)) => {
+                println!("{} : {}", err.extended_code, err_code);
+                assert_eq!(err.extended_code, err_code);
+                let msg = msg.unwrap();
+                println!("{} : {}", msg, err_msg);
+                assert_eq!(msg, err_msg);
+            }
+            _ => panic!("failed to assert_err"),
+        }
+    }
+
+    #[test]
+    fn test_db_constraints() {
+        let conn = get_test_conn();
+
+        conn.client
+            .execute("INSERT INTO categories (name) VALUES ('hello')", ())
+            .unwrap();
+
+        assert_err(
+            conn.client
+                .execute("INSERT INTO categories (name) VALUES ('hello')", ()),
+            ffi::SQLITE_CONSTRAINT_UNIQUE,
+            "UNIQUE constraint failed: categories.name",
+        );
+
+        assert_err(
+            conn.client.execute(
+                "INSERT INTO categories (name) VALUES (?)",
+                [Option::<String>::None],
+            ),
+            ffi::SQLITE_CONSTRAINT_NOTNULL,
+            "NOT NULL constraint failed: categories.name",
+        );
+
+        assert_err(
+            conn.client.execute(
+                "INSERT INTO tasks (info) VALUES (?)",
+                [Option::<String>::None],
+            ),
+            ffi::SQLITE_CONSTRAINT_NOTNULL,
+            "NOT NULL constraint failed: tasks.info",
+        );
+
+        assert_err(
+            conn.client
+                .execute("INSERT INTO categories (name) VALUES ('')", ()),
+            ffi::SQLITE_CONSTRAINT_CHECK,
+            "CHECK constraint failed: name != ''",
+        );
+
+        assert_err(
+            conn.client
+                .execute("INSERT INTO tasks (info) VALUES ('')", ()),
+            ffi::SQLITE_CONSTRAINT_CHECK,
+            "CHECK constraint failed: info != ''",
+        );
+
+        assert_err(
+            conn.client
+                .execute("INSERT INTO tasks (info, done) VALUES ('foo', ?)", [2]),
+            ffi::SQLITE_CONSTRAINT_CHECK,
+            "CHECK constraint failed: done IN (0,1)",
+        );
+
+        assert!(
+            conn.client
+                .execute(
+                    "INSERT INTO tasks (info, due_date) VALUES ('bar', ?)",
+                    [Option::<String>::None]
+                )
+                .is_ok()
+        );
+
+        assert_err(
+            conn.client.execute(
+                "INSERT INTO tasks (info, due_date) VALUES ('bar', ?)",
+                ["111-1-1".to_string()],
+            ),
+            ffi::SQLITE_CONSTRAINT_CHECK,
+            "CHECK constraint failed: due_date IS NULL OR
+                    (due_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]' AND
+                        date(due_date) IS NOT NULL)",
+        );
+
+        assert_err(
+            conn.client.execute(
+                "INSERT INTO tasks (info, due_date) VALUES ('bar', ?)",
+                ["2025-13-01".to_string()],
+            ),
+            ffi::SQLITE_CONSTRAINT_CHECK,
+            "CHECK constraint failed: due_date IS NULL OR
+                    (due_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]' AND
+                        date(due_date) IS NOT NULL)",
+        );
+
+        // and make sure to split up these tests
+        // fn test_db_constraints() { let conn = get_test_conn(); conn.client .execute("INSERT INTO categories (name) VALUES ('hello')", ()) .unwrap(); assert_err( conn.client .execute("INSERT INTO categories (name) VALUES ('hello')", ()), ffi::SQLITE_CONSTRAINT_UNIQUE, "UNIQUE constraint failed: categories.name", ); assert_err( conn.client.execute( "INSERT INTO categories (name) VALUES (?)", [Option::<String>::None], ), ffi::SQLITE_CONSTRAINT_NOTNULL, "NOT NULL constraint failed: categories.name", ); assert_err( conn.client.execute( "INSERT INTO tasks (info) VALUES (?)", [Option::<String>::None], ), ffi::SQLITE_CONSTRAINT_NOTNULL, "NOT NULL constraint failed: tasks.info", ); assert_err( conn.client .execute("INSERT INTO categories (name) VALUES ('')", ()), ffi::SQLITE_CONSTRAINT_CHECK, "CHECK constraint failed: name != ''", ); assert_err( conn.client .execute("INSERT INTO tasks (info) VALUES ('')", ()), ffi::SQLITE_CONSTRAINT_CHECK, "CHECK constraint failed: info != ''", ); // assert!( // conn.client // .execute("INSERT INTO tasks (info, done) VALUES ('foo', ?)", [2],) // .is_err() // ); // // assert!( // conn.client // .execute( // "INSERT INTO tasks (info, due_date) VALUES ('bar', ?)", // [Option::<String>::None] // ) // .is_ok() // ); // // assert!( // conn.client // .execute( // "INSERT INTO tasks (info, due_date) VALUES ('bar', ?)", // ["111-1-1".to_string()] // ) // .is_err() // ); // // assert!( // conn.client // .execute( // "INSERT INTO tasks (info, due_date) VALUES ('bar', ?)", // ["2025-13-01".to_string()] // ) // .is_err() // ); } finish this test
+    }
+
+    fn get_test_conn() -> Conn {
+        Conn::build(":memory:").unwrap()
+    }
+
+    #[test]
+    fn open_conn() {
+        assert_eq!(
+            get_test_conn()
+                .client
+                .query_row("SELECT 1 + 1", (), |row| row.get::<_, i32>(0))
+                .unwrap(),
+            2
+        );
+    }
+
+    #[test]
+    fn valid_insertion() {
+        let conn = get_test_conn();
+
+        conn.client
+            .execute("INSERT INTO categories (name) VALUES ('test')", ())
+            .unwrap();
+
+        conn.client
+            .execute(
+                r"
+                INSERT INTO tasks (info, due_date, category)
+                VALUES (
+                    'This is a test',
+                    '2025-09-23',
+                    (SELECT id FROM categories WHERE name = 'test')
+                )",
+                (),
+            )
+            .unwrap();
+
+        struct TestTask {
+            id: usize,
+            info: String,
+            done: bool,
+            due_date: String,
+            category: String,
+        }
+
+        let row = conn
+            .client
+            .query_row(
+                r"
+                SELECT tasks.id, tasks.info, tasks.done, tasks.due_date, categories.name
+                FROM tasks
+                JOIN categories ON tasks.category = categories.id
+                ",
+                (),
+                |row| {
+                    Ok(TestTask {
+                        id: row.get(0)?,
+                        info: row.get(1)?,
+                        done: row.get(2)?,
+                        due_date: row.get(3)?,
+                        category: row.get(4)?,
+                    })
+                },
+            )
+            .unwrap();
+
+        assert_eq!(row.id, 1);
+        assert_eq!(row.info, "This is a test".to_string());
+        assert_eq!(row.done, false);
+        assert_eq!(row.due_date, "2025-09-23".to_string());
+        assert_eq!(row.category, "test".to_string());
+    }
+}
+
 // use chrono::NaiveDate;
 // use rusqlite::{Connection, Result};
 //
@@ -6,35 +292,6 @@
 // }
 //
 // impl TodoConn {
-//     pub fn build(db_path: &str) -> Result<Self> {
-//         let conn = Connection::open(db_path)?;
-//         conn.execute(
-//             r#"
-//             CREATE TABLE IF NOT EXISTS categories (
-//                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-//                 name TEXT NOT NULL UNIQUE CHECK(name != '')
-//             )
-//             "#,
-//             (),
-//         )?;
-//         conn.execute(
-//             r#"
-//             CREATE TABLE IF NOT EXISTS tasks (
-//                 id INTEGER PRIMARY KEY,
-//                 info TEXT NOT NULL UNIQUE CHECK(info != ''),
-//                 done BOOLEAN NOT NULL DEFAULT false CHECK(done in (0, 1)),
-//                 due_date TEXT CHECK(
-//                     due_date IS NULL OR
-//                     (due_date GLOB '[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9]' AND
-//                         date(due_date) IS NOT NULL)
-//             ),
-//             category INTEGER,
-//             FOREIGN KEY(category) REFERENCES categories(id)
-//             )"#,
-//             (),
-//         )?;
-//         Ok(TodoConn { client: conn })
-//     }
 //
 //     pub fn add_task(
 //         &self,
@@ -226,66 +483,6 @@
 //     // }
 // }
 //
-// pub fn format_date(date: &str, today: &NaiveDate) -> Result<String, &'static str> {
-//     let cleaned = date.trim().replace("/", "-");
-//     let parts: Vec<_> = cleaned.trim().split('-').collect();
-//
-//     // let parse_part = |part: &str, msg| part.parse::<u32>().map_err(|_| msg);
-//     // let (y, m, d) = match parts.len() {
-//     //     3 => (
-//     //         Some(parse_part(parts[0], "Y-M-D invalid year")?),
-//     //         Some(parse_part(parts[1], "Y-M-D invalid month")?),
-//     //         Some(parse_part(parts[2], "Y-M-D invalid day")?),
-//     //     ),
-//     //     2 => (
-//     //         None,
-//     //         Some(parse_part(parts[0], "M-D invalid month")?),
-//     //         Some(parse_part(parts[1], "M-D invalid day")?),
-//     //     ),
-//     //     1 => (None, None, Some(parse_part(parts[0], "D invalid day")?)),
-//     //     _ => return Err("bad number of parts"),
-//     // };
-//     //
-//     Ok(cleaned)
-//
-//     // fn make_date(y: i32, m: u32, d: u32) -> Result<String, &'static str> {
-//     //     if let Some(date) = NaiveDate::from_ymd_opt(y, m, d) {
-//     //         Ok(date.format("%Y-%m-%d").to_string())
-//     //     } else {
-//     //         Err("unable to parse date")
-//     //     }
-//     // }
-//     //
-//     // match (y, m, d) {
-//     //     // wont work past 2100 :(, will fix then
-//     //     (Some(year), Some(month), Some(day)) => make_date(
-//     //         (if year < 100 { year + 2000 } else { year }) as i32,
-//     //         month,
-//     //         day,
-//     //     ),
-//     //     (None, Some(month), Some(day)) => {
-//     //         if month > today.month() || (month == today.month() && day >= today.day()) {
-//     //             make_date(today.year(), month, day)
-//     //         } else {
-//     //             make_date(today.year() + 1, month, day)
-//     //         }
-//     //     }
-//     //     (None, None, Some(day)) => {
-//     //         if day >= today.day() {
-//     //             make_date(today.year(), today.month(), day)
-//     //         } else {
-//     //             let today_month = today.month();
-//     //             let (new_year, new_month) = if today_month == 12 {
-//     //                 (today.year() + 1, 1)
-//     //             } else {
-//     //                 (today.year(), today_month + 1)
-//     //             };
-//     //             make_date(new_year, new_month, day)
-//     //         }
-//     //     }
-//     //     _ => Err("cannot parse the date"),
-//     // }
-// }
 //
 // // //look into chrono for this
 // // //read env for time zones change time to my time
@@ -319,3 +516,5 @@
 // // make custom err type
 // //rust cli to a supabase db
 // // makee sujre to add category
+//
+// write alot of documentation
--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,100 @@
-// use clap::{Parser, Subcommand};
-// use std::process;
-// use todo::TodoConn;
-//
-// ///A command line todo app
-// #[derive(Debug, Parser)]
-// #[command(name = "todo")]
-// #[command(about = "A command line todo app", long_about = None)]
-// struct Cli {
-//     #[command(subcommand)]
-//     command: Commands,
-// }
-//
-// #[derive(Debug, Subcommand)]
-// enum Commands {
-//     /// Add todo list item
-//     #[command(arg_required_else_help = true)]
-//     Add {
-//         /// Task to add
-//         task: String,
-//
-//         ///  Category of the task
-//         #[arg(short, long)]
-//         category: Option<String>,
-//
-//         /// Due date: YYYY-MM-DD, MM-DD, DD (slashes allowed, leading zeros optional)
-//         #[arg(short, long)]
-//         due_date: Option<String>,
-//     },
-//
-//     /// List all todo items
-//     List {
-//         /// Sort By Category
-//         #[arg(short, long, action = clap::ArgAction::SetTrue)]
-//         category: bool,
-//
-//         /// Include Finshed Tasks
-//         #[arg(short, long, action = clap::ArgAction::SetTrue)]
-//         include_done: bool,
-//     },
-//
-//     /// Edit todo list item
-//     #[command(arg_required_else_help = true)]
-//     Edit {
-//         /// Id of task to finish
-//         id: i32,
-//
-//         /// Set if problems is done with true or false
-//         #[arg(short, long)]
-//         finish: Option<bool>,
-//
-//         /// Set problem due date
-//         #[arg(short, long)]
-//         due_date: Option<String>,
-//
-//         /// Set problem category
-//         #[arg(short, long)]
-//         category: Option<String>,
-//
-//         /// Set problem info
-//         #[arg(short, long)]
-//         info: Option<String>,
-//
-//         /// Remove problem (use with caution, must write 'delete')
-//         #[arg(short, long, action = clap::ArgAction::SetTrue)]
-//         remove: bool,
-//     },
-// }
-//
-// fn main() {
-//     let conn = TodoConn::build("./todo.db").unwrap_or_else(|e| {
-//         eprintln!("Could not init db: {e}");
-//         process::exit(1)
-//     });
-//
-//     // print a success message for each
-//     // also print current time and date
-//
-//     match Cli::parse().command {
-//         Commands::Add {
-//             task,
-//             category,
-//             due_date,
-//         } => conn
-//             .add_task(&task, category.as_deref(), due_date.as_deref())
-//             .unwrap_or_else(|e| {
-//                 eprintln!("Could not add task: {e}");
-//                 process::exit(1)
-//             }),
-//         Commands::List {
-//             category,
-//             include_done,
-//         } => conn
-//             .get_task(category, include_done)
-//             .unwrap_or_else(|e| {
-//                 println!("Could not get tasks: {e}");
-//                 process::exit(1)
-//             })
-//             .iter()
-//             .for_each(|t| println!("{t}")),
-//         Commands::Edit {
-//             id,
-//             finish,
-//             due_date,
-//             category,
-//             info,
-//             remove,
-//         } => conn
-//             .edit_task(
-//                 id,
-//                 finish,
-//                 due_date.as_deref(),
-//                 category.as_deref(),
-//                 info.as_deref(),
-//                 remove,
-//             )
-//             .unwrap_or_else(|e| {
-//                 println!("Could not edit task: {e}");
-//                 process::exit(1)
-//             }),
-//     }
-// }
+use chrono::Local;
+use clap::{Parser, Subcommand};
+use todo::Conn;
+
+///A command line todo app
+#[derive(Debug, Parser)]
+#[command(name = "todo")]
+#[command(about = "A command line todo app", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Add todo list item
+    #[command(arg_required_else_help = true)]
+    Add {
+        /// Task to add
+        task: String,
+
+        ///  Category of the task
+        #[arg(short, long)]
+        category: Option<String>,
+
+        /// Due date: YYYY-MM-DD, MM-DD, DD (slashes allowed, leading zeros optional)
+        #[arg(short, long)]
+        due_date: Option<String>,
+    },
+
+    /// List all todo items
+    List {
+        /// Sort By Category
+        #[arg(short, long, action = clap::ArgAction::SetTrue)]
+        category: bool,
+
+        /// Include Finshed Tasks
+        #[arg(short, long, action = clap::ArgAction::SetTrue)]
+        include_done: bool,
+    },
+
+    /// Edit todo list item
+    #[command(arg_required_else_help = true)]
+    Edit {
+        /// Id of task to finish
+        id: i32,
+
+        /// Set if problems is done with true or false
+        #[arg(short, long)]
+        finish: Option<bool>,
+
+        /// Set problem due date
+        #[arg(short, long)]
+        due_date: Option<String>,
+
+        /// Set problem category
+        #[arg(short, long)]
+        category: Option<String>,
+
+        /// Set problem info
+        #[arg(short, long)]
+        info: Option<String>,
+
+        /// Remove problem (use with caution, must write 'delete')
+        #[arg(short, long, action = clap::ArgAction::SetTrue)]
+        remove: bool,
+    },
+}
+
+fn main() {
+    let today = Local::now().date_naive();
+    println!("Welcome to todo: {today}");
+
+    let conn = Conn::build("./todo.db").unwrap_or_else(|err| {
+        eprintln!("Could not acess db: {err}");
+        std::process::exit(1)
+    });
+
+    match Cli::parse().command {
+        Commands::Add {
+            task,
+            category,
+            due_date,
+        } => println!("Add command"),
+        Commands::List {
+            category,
+            include_done,
+        } => println!("List command"),
+        Commands::Edit {
+            id,
+            finish,
+            due_date,
+            category,
+            info,
+            remove,
+        } => println!("Edit command"),
+    }
+
+    println!("Operation was a Success")
+}
